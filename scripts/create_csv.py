f4ae27693c2a8874ce82169db5aa8cb63801f059
Leoprvn <bravopraveen92@gmail.com>
2020-09-29 01:36:04 +1000

    Adding the script file

---
+++ scripts/create_csv.py
@@ -0,0 +1,87 @@
+import csv
+import random
+import datetime
+import sys
+
+
+def arg_validate():
+    """
+    Validate the anumber of argument
+    :return: if validateion fails, comes out of the script
+    """
+    if len(sys.argv) < 2:
+        print("Please pass valid arguments")
+        print("Execute: python create_csv.py  <file_input> <number_of_lines>")
+        print("Example: python create_csv.py 'csv_file.csv' 100")
+        sys.exit(1)
+
+
+def random_first_last_name_dob(type_char):
+    """
+    Generate the random first name or last name or address or date of birth
+    :param type_char: 'f' - first_name ,'l' - last_name,'a' - address ,'n' - date_of_birth
+    :return: Random First name/Last name/ Address / Data of Birth based on the parameter
+           : "Praveen' / 'Hill' / '8502 Madrone Avenue' / '22/11/1992'
+    :example: random_first_last_name_dob(<type_char>
+            : random_first_last_name_dob('f')
+    """
+    first_name = ('Andrew', 'John', 'Clifford', 'Jinna', 'Andy', 'Joe', 'Joesph', 'Thomas', 'Praveen', 'Edward')
+    last_name = ('Johnson', 'Smith', 'Williams', 'Robinson', 'Walker', 'Allen', 'Hill', 'Scott', 'Adams', 'Carter')
+    address = ('1745 T Street Southeast', '6007 Applegate Lane', '560 Penstock Drive', '150 Carter Street',
+               '2721 Lindsay Avenue', '18 Densmore Drive', '1364 Capri Drive', '159 Downey Drive', '109 Summit Street',
+               '8502 Madrone Avenue')
+    year = ('1990','1992','1993','1994')
+    try:
+        if type_char == "f":
+            choice_anc = random.choice(first_name)
+        elif type_char == "l":
+            choice_anc = random.choice(last_name)
+        elif type_char == "a":
+            choice_anc = random.choice(address)
+        elif type_char == "n":
+            date_str = str(random.randint(1,28))+"/"+str(random.randint(1,12))+"/"+str(random.choice(year))
+            choice_anc = datetime.datetime.strptime(date_str, '%d/%m/%Y').date()
+        else:
+            print("Invalid type of characters choice")
+        return choice_anc
+    except:
+        print("I/O Error")
+        sys.exit(1)
+
+
+def create_csv_file(file_name, number_of_lines):
+    """
+    create the csv file with the random first_name,last_name,address,date_of_birth
+    :param file_name: provide the file_name which needs to be created
+    :param number_of_lines: number of lines needs ot be generator
+    :return: create a csv file
+    :example: create_csv_file('random_csv_file.csv',10)
+    """
+    column_name = ['first_name','last_name','address','date_of_birth']
+    try:
+        with open(file_name, "w+",newline="") as csv_file:
+            file_writer = csv.DictWriter(csv_file,fieldnames=column_name,delimiter=',')
+            file_writer.writeheader()
+            for i in range(number_of_lines):
+                file_writer.writerow({column_name[0]:random_first_last_name_dob('f'),column_name[1]: \
+                    random_first_last_name_dob('l'),column_name[2]:random_first_last_name_dob('a'),\
+                                      column_name[3]:random_first_last_name_dob('n')})
+        print("File {} has been created Successfully with {} lines ".format(file_name,number_of_lines))
+    except:
+        print("Error in Creating File {}".format(file_name))
+        sys.exit(1)
+
+
+def main():
+    arg_validate()
+    csv_file_input = str(sys.argv[1])
+    number_of_lines = int(sys.argv[2])
+    try:
+        create_csv_file(csv_file_input,number_of_lines)
+    except:
+        print("main function failed")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ scripts/create_flat_file.py
@@ -0,0 +1,64 @@
+import random
+import string
+import sys
+
+
+def arg_validate():
+    if len(sys.argv) < 3:
+        print("Please pass valid arguments")
+        print("Execution: python create_flat_file.py  <flat_file_input>  <number_of_lines_to_be_generated> \
+        <columnoffset>")
+        print("Example: create_flat_file.py 'flat_file.txt' 10 '5,12,3,2,13,7,10,13,20,13'")
+        sys.exit(1)
+
+
+def get_random_alphanumeric_character(length):
+    """
+    Return the random alphanumberic value based on the characeter length given to the parameter
+    Parameter: length of character
+    Example: get_random_alphanumeric_character(10)
+    Returns: 'Pjt5BiY43G' #random value
+    """
+    try:
+        alphanumeric_character = string.ascii_letters + string.digits
+        result_str = ''.join((random.choice(alphanumeric_character) for i in range(int(length))))
+        return result_str
+    except:
+        print("I/O Error")
+        sys.exit(1)
+
+
+def create_flat_file(file_name, number_of_lines, tot_num_char):
+    """
+    Create the flat file with random alphanumberic characters based on the number of character
+    Parameters: file_name with complete path, number of lines, total number of characters to be generator
+    Example: create_flat_file(r"/tmp/input_file.txt",10,98)
+    Returns: create a file with random alphnumberic values with the mentioned characters per mentioned number of lines
+    """
+    try:
+        with open(file_name, "w+") as f:
+            for i in range(number_of_lines):
+                val = get_random_alphanumeric_character(tot_num_char)
+                f.write(val + "\n")
+        print("File {} has been created Successfully with {} lines & {} characters per line"\
+              .format(file_name,number_of_lines,tot_num_char))
+    except:
+        print("Error in Creating File {}".format(file_name))
+        sys.exit(1)
+
+
+def main():
+    arg_validate()
+    flat_file_input = str(sys.argv[1])
+    number_of_records = int(sys.argv[2])
+    offset = [int(i) for i in str(sys.argv[3]).split(',')]
+    tot_num_char = sum(offset)
+    try:
+        create_flat_file(flat_file_input, number_of_records, tot_num_char)
+    except:
+        print('Exiting the main script')
+
+
+if __name__ == "__main__":
+    main()
+
