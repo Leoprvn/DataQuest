d603433c9a3213cd7be61676d25c7999d695c4f9
Leoprvn <bravopraveen92@gmail.com>
2020-09-30 18:04:31 +1000

    Update create_csv.py
    Updating the code as per PEP standards

--- test/create_csv.py
+++ test/create_csv.py
@@ -1,7 +1,11 @@
-import csv
-import random
-import datetime
+"""
+This module to create csv file with the parameter as file name and number
+of lines
+"""
 import sys
+import datetime
+import random
+import csv
 
 
 def arg_validate():
@@ -25,28 +29,28 @@ def random_first_last_name_dob(type_char):
     :example: random_first_last_name_dob(<type_char>
             : random_first_last_name_dob('f')
     """
-    first_name = ('Andrew', 'John', 'Clifford', 'Jinna', 'Andy', 'Joe', 'Joesph', 'Thomas', 'Praveen', 'Edward')
-    last_name = ('Johnson', 'Smith', 'Williams', 'Robinson', 'Walker', 'Allen', 'Hill', 'Scott', 'Adams', 'Carter')
-    address = ('1745 T Street Southeast', '6007 Applegate Lane', '560 Penstock Drive', '150 Carter Street',
-               '2721 Lindsay Avenue', '18 Densmore Drive', '1364 Capri Drive', '159 Downey Drive', '109 Summit Street',
-               '8502 Madrone Avenue')
+    first_name = ('Andrew', 'John', 'Clifford', 'Jinna', 'Andy'
+                  , 'Joe', 'Joesph', 'Thomas', 'Praveen', 'Edward')
+    last_name = ('Johnson', 'Smith', 'Williams', 'Robinson', 'Walker'
+                 , 'Allen', 'Hill', 'Scott', 'Adams', 'Carter')
+    address = ('1745 T Street Southeast', '6007 Applegate Lane', '560 Penstock Drive'
+               , '150 Carter Street','2721 Lindsay Avenue', '18 Densmore Drive'
+               , '1364 Capri Drive', '159 Downey Drive', '109 Summit Street'
+               , '8502 Madrone Avenue')
     year = ('1990','1992','1993','1994')
-    try:
-        if type_char == "f":
-            choice_anc = random.choice(first_name)
-        elif type_char == "l":
-            choice_anc = random.choice(last_name)
-        elif type_char == "a":
-            choice_anc = random.choice(address)
-        elif type_char == "n":
-            date_str = str(random.randint(1,28))+"/"+str(random.randint(1,12))+"/"+str(random.choice(year))
-            choice_anc = datetime.datetime.strptime(date_str, '%d/%m/%Y').date()
-        else:
-            print("Invalid type of characters choice")
-        return choice_anc
-    except:
-        print("I/O Error")
-        sys.exit(1)
+    if type_char == "f":
+        choice_anc = random.choice(first_name)
+    elif type_char == "l":
+        choice_anc = random.choice(last_name)
+    elif type_char == "a":
+        choice_anc = random.choice(address)
+    elif type_char == "n":
+        date_str = str(random.randint(1,28)) + "/" + str(random.randint(1,12))\
+                       + "/" + str(random.choice(year))
+        choice_anc = datetime.datetime.strptime(date_str, '%d/%m/%Y').date()
+    else:
+        print("Invalid type of characters choice")
+    return choice_anc
 
 
 def create_csv_file(file_name, number_of_lines):
@@ -58,30 +62,32 @@ def create_csv_file(file_name, number_of_lines):
     :example: create_csv_file('random_csv_file.csv',10)
     """
     column_name = ['first_name','last_name','address','date_of_birth']
-    try:
-        with open(file_name, "w+",newline="") as csv_file:
-            file_writer = csv.DictWriter(csv_file,fieldnames=column_name,delimiter=',')
-            file_writer.writeheader()
-            for i in range(number_of_lines):
-                file_writer.writerow({column_name[0]:random_first_last_name_dob('f'),column_name[1]: \
-                    random_first_last_name_dob('l'),column_name[2]:random_first_last_name_dob('a'),\
-                                      column_name[3]:random_first_last_name_dob('n')})
-        print("File {} has been created Successfully with {} lines ".format(file_name,number_of_lines))
-    except:
-        print("Error in Creating File {}".format(file_name))
-        sys.exit(1)
+    with open(file_name, "w+",newline="") as csv_file:
+        file_writer = csv.DictWriter(csv_file,fieldnames=column_name,delimiter=',')
+        file_writer.writeheader()
+        total_lines = 0
+        for line_number in range(number_of_lines):
+            file_writer.writerow({column_name[0]:random_first_last_name_dob('f')
+                                     ,column_name[1]:random_first_last_name_dob('l')
+                                     ,column_name[2]:random_first_last_name_dob('a')
+                                     , column_name[3]:random_first_last_name_dob('n')})
+            total_lines += line_number
+    print("File {} has been created Successfully with {} lines ".format(file_name,total_lines))
 
 
 def main():
+    """
+    this function is to call the required function to create a csv file
+    :return:
+    """
     arg_validate()
     csv_file_input = str(sys.argv[1])
     number_of_lines = int(sys.argv[2])
     try:
         create_csv_file(csv_file_input,number_of_lines)
-    except:
+    except RuntimeError:
         print("main function failed")
 
 
 if __name__ == "__main__":
     main()
-
