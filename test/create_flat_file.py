87519c2202686205b12657d98d7ddd500a802fec
Leoprvn <bravopraveen92@gmail.com>
2020-09-30 18:05:23 +1000

    Update create_flat_file.py
    Updating the code as per PEP standards

--- test/create_flat_file.py
+++ test/create_flat_file.py
@@ -1,14 +1,23 @@
-import random
-import string
+"""
+This module is to create the flat file based on the number of lines and
+column offset value given
+"""
 import sys
+import string
+import random
 
 
 def arg_validate():
+    """
+    To Validate the argument passed to the function
+    :return: None
+    """
     if len(sys.argv) < 3:
         print("Please pass valid arguments")
-        print("Execution: python create_flat_file.py  <flat_file_input>  <number_of_lines_to_be_generated> \
-        <columnoffset>")
-        print("Example: create_flat_file.py '/opt/io_folder/flat_file.txt' 10 '5,12,3,2,13,7,10,13,20,13'")
+        print("Execution: python create_flat_file.py  <flat_file_input> \
+              <number_of_lines_to_be_generated> <columnoffset>")
+        print("Example: create_flat_file.py '/opt/io_folder/flat_file.txt' \
+         10 '5,12,3,2,13,7,10,13,20,13'")
         sys.exit(1)
 
 
@@ -19,35 +28,36 @@ def get_random_alphanumeric_character(length):
     Example: get_random_alphanumeric_character(10)
     Returns: 'Pjt5BiY43G' #random value
     """
-    try:
-        alphanumeric_character = string.ascii_letters + string.digits
-        result_str = ''.join((random.choice(alphanumeric_character) for i in range(int(length))))
-        return result_str
-    except:
-        print("I/O Error")
-        sys.exit(1)
+    alphanumeric_character = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(alphanumeric_character) for i in range(int(length))))
+    return result_str
 
 
 def create_flat_file(file_name, number_of_lines, tot_num_char):
     """
-    Create the flat file with random alphanumberic characters based on the number of character
-    Parameters: file_name with complete path, number of lines, total number of characters to be generator
+    Create the flat file with random alphanumberic characters based on
+    the number of character
+    Parameters: file_name with complete path, number of lines, total number
+    of characters to be generator
     Example: create_flat_file(r"/tmp/input_file.txt",10,98)
-    Returns: create a file with random alphnumberic values with the mentioned characters per mentioned number of lines
+    Returns: create a file with random alphnumberic values with the
+    mentioned characters per mentioned number of lines
     """
-    try:
-        with open(file_name, "w+") as f:
-            for i in range(number_of_lines):
-                val = get_random_alphanumeric_character(tot_num_char)
-                f.write(val + "\n")
-        print("File {} has been created Successfully with {} lines & {} characters per line"\
-              .format(file_name,number_of_lines,tot_num_char))
-    except:
-        print("Error in Creating File {}".format(file_name))
-        sys.exit(1)
+    with open(file_name, "w+") as file:
+        nnumber_lines = 0
+        for line_number in range(number_of_lines):
+            val = get_random_alphanumeric_character(tot_num_char)
+            file.write(val + "\n")
+            nnumber_lines += line_number
+    print("File {} has been created Successfully with {} lines & {} characters per line"\
+          .format(file_name,number_of_lines,tot_num_char))
 
 
 def main():
+    """
+     This function calls the required function to create a flat file.
+    :return: None
+    """
     arg_validate()
     flat_file_input = str(sys.argv[1])
     number_of_records = int(sys.argv[2])
@@ -55,10 +65,9 @@ def main():
     tot_num_char = sum(offset)
     try:
         create_flat_file(flat_file_input, number_of_records, tot_num_char)
-    except:
+    except RuntimeError:
         print('Exiting the main script')
 
 
 if __name__ == "__main__":
     main()
-
